076beade489bb0dedd9ac58f9820436f62e931de
noooda <noooda211@gmail.com>
2025-07-25 08:16:48 +0900

    feat: Create empty project from initializer

---
+++ src/test/java/io/github/noooda/restpra/RestpraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.noooda.restpra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestpraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
